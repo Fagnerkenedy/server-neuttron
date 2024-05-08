2d94c2813426f44dd37e58f2fa08cd8bcc8b7ca5
Fagner Kenedy <fagnerkenedy@hotmail.com>
2024-05-08 20:29:18 -0300

    Adiciona moduleNameApi para módulos que tem espaço no nome

--- src/controllers/module/index.js
+++ src/controllers/module/index.js
@@ -11,7 +11,7 @@ module.exports = {
                 return res.status(400).json({ error: "Missing orgId or moduleName in request" });
             }
             connection = await mysql.createConnection({ ...dbConfig, database: `${orgId}` });
-            let moduleNameApi = moduleName.replace(/[^\w\s]|[\sç]/gi, '_').toLowerCase();
+            let moduleNameApi = moduleName.replace(/[^\w\s]|[\sç]/gi, '_')
             await connection.beginTransaction();
             const query = `CREATE TABLE IF NOT EXISTS ${moduleNameApi} (
                 id VARCHAR(19) PRIMARY KEY,
@@ -22,14 +22,15 @@ module.exports = {
             const query2 = `CREATE TABLE IF NOT EXISTS modules (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 name VARCHAR(255),
+                api_name VARCHAR(255),
                 perfil VARCHAR(2000),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )`;
             await connection.execute(query2);
             await connection.execute(
-                'INSERT INTO modules (name) VALUES (?);',
-                [moduleName]
+                'INSERT INTO modules (name, api_name) VALUES (?, ?);',
+                [moduleName, moduleNameApi]
             );
             const [result] = await connection.execute(query);
 
@@ -66,7 +67,7 @@ module.exports = {
             const orgId = req.params.org
             // const moduleName = req.params.module
             const connection = await mysql.createConnection({ ...dbConfig, database: `${orgId}` });
-            const query = `SELECT name FROM modules;`
+            const query = `SELECT name, api_name FROM modules;`
             const [result] = await connection.execute(query);
             await connection.end();
             res.json({ success: true, message: "Module Name", result });
@@ -93,8 +94,8 @@ module.exports = {
             const orgId = req.params.org
             const moduleName = req.body.name
             const newName = req.body.new_name
-            let moduleNameApi = moduleName.replace(/[^\w\s]|[\sç]/gi, '_').toLowerCase();
-            let newModuleNameApi = newName.replace(/[^\w\s]|[\sç]/gi, '_').toLowerCase();
+            let moduleNameApi = moduleName.replace(/[^\w\s]|[\sç]/gi, '_')
+            let newModuleNameApi = newName.replace(/[^\w\s]|[\sç]/gi, '_')
             const connection = await mysql.createConnection({ ...dbConfig, database: `${orgId}` });
             const row = await connection.execute('UPDATE modules SET name = ? WHERE name = ?;', [newName, moduleName]);
             await connection.execute(`ALTER TABLE ${moduleNameApi} RENAME TO ${newModuleNameApi};`);
@@ -109,7 +110,7 @@ module.exports = {
             const orgId = req.params.org
             const moduleName = req.body.name
             const connection = await mysql.createConnection({ ...dbConfig, database: `${orgId}` });
-            let moduleNameApi = moduleName.replace(/[^\w\s]|[\sç]/gi, '_').toLowerCase();
+            let moduleNameApi = moduleName.replace(/[^\w\s]|[\sç]/gi, '_')
             //const row = await connection.execute('DELETE FROM modules WHERE module_id = ? AND organization_id = ?; DELETE FROM module_fields WHERE module_id = ? AND organization_id = ?; DELETE FROM module_data WHERE module_id = ? AND organization_id = ?;', [module_id, orgId, module_id, orgId, module_id, orgId]);
             const deleteModuleQuery = 'DELETE FROM modules WHERE name = ?;';
             const deleteFieldsQuery = 'DELETE FROM fields WHERE module = ?;';

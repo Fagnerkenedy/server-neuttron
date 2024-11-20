c3f0b9c11e1f0be90b29637cdef9774867a97b38
Fagnerkenedy <fagnerkenedy@hotmail.com>
2024-11-20 18:58:30 -0300

    Adiciona /notifications

--- src/controllers/notifications/index.js
+++ src/controllers/notifications/index.js
@@ -21,7 +21,7 @@ module.exports = {
 
             // console.log("received notifications header: ", req.header)
             // console.log("received notifications params: ", req.params)
-            console.log("received notifications: ", JSON.stringify(req))
+            console.log("received notifications: ", req)
             console.log("process.env.ACCESS_TOKEN_MERCADO_PAGO: ", process.env.ACCESS_TOKEN_MERCADO_PAGO)
 
             mercadopago.payment.get(req.body.resource)

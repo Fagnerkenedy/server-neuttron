6a7c317bc5f4b14f9a22d524ce8c8811259d1932
Fagnerkenedy <fagnerkenedy@hotmail.com>
2025-01-08 08:39:44 -0300

    Adicionando socket io

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const router = express.Router();
+// const router = express.Router();
 const cors = require("cors");
 const chatController = require("../controllers/chat")
 
-router.use(express.urlencoded({ extended: false }));
-router.use(express.json());
-router.use(cors());
+module.exports = (io) => {
+    const router = express.Router();
 
-router.get('/', (req, res) => {res.status(200).json( {success: true, message: "Nothing to see here!"})})
+    router.use(express.urlencoded({ extended: false }));
+    router.use(express.json());
+    router.use(cors());
 
-router.post("/send-message", chatController.sendMessage);
+    router.get('/', (req, res) => {
+        res.status(200).json({ success: true, message: "Nothing to see here!" });
+    });
 
-module.exports = router
+    router.post("/send-message", (req, res) => chatController.sendMessage(req, res, io));
+
+    return router;
+};
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const router = express.Router();
+// const router = express.Router();
 const cors = require("cors");
 const messagesController = require("../controllers/messages")
 
-router.use(express.urlencoded({ extended: false }));
-router.use(express.json());
-router.use(cors());
+module.exports = (io) => {
+    const router = express.Router();
 
-router.get('/', (req, res) => {res.status(200).json( {success: true, message: "Nothing to see here!"})})
-
-router.post("/webhook", messagesController.webhookPost);
-router.get("/webhook", messagesController.webhookGet);
-
-module.exports = router
+    router.use(express.urlencoded({ extended: false }));
+    router.use(express.json());
+    router.use(cors());
+    
+    router.get('/', (req, res) => {res.status(200).json( {success: true, message: "Nothing to see here!"})})
+    
+    router.post("/webhook", (req, res) => messagesController.webhookPost(req, res, io));
+    router.get("/webhook", messagesController.webhookGet);
+    return router
+}

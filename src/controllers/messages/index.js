94dc3341c8395615909a03396012b60006c44de9
Fagnerkenedy <fagnerkenedy@hotmail.com>
2025-01-31 17:21:20 -0300

    Atualiza bancos de dados

--- src/controllers/messages/index.js
+++ src/controllers/messages/index.js
@@ -225,19 +225,24 @@ module.exports = {
             console.log("io jsonData.text.body:", jsonData?.text?.body)
             console.log("io value.messages[0].timestamp:", value.messages[0].timestamp)
             console.log("io conversationId:", conversationId)
+            const isoString = new Date(value.messages[0].timestamp * 1000).toISOString().replace('T', ' ').replace('Z', '')
             if (message) {
                 io.to(`org${orgId}`).emit('newMessage', {
                     senderName: contactName,
                     body,
                     timestamp: value.messages[0].timestamp,
-                    conversationId
+                    conversationId,
+                    last_message: body,
+                    updated_at: isoString
                 });
                 if (bot.length > 0) {
                     io.to(`org${orgId}`).emit('newMessage', {
                         senderName: name,
                         body: jsonData.text.body,
                         timestamp: value.messages[0].timestamp,
-                        conversationId
+                        conversationId,
+                        last_message: jsonData.text.body,
+                        updated_at: isoString
                     });
                 }
             }

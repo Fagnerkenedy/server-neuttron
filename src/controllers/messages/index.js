6d774e70c9b2ab8218bb2bff2e515ec730b28064
Fagnerkenedy <fagnerkenedy@hotmail.com>
2025-02-10 00:16:25 -0300

    Ajusta envio de pdf no chatbot

--- src/controllers/messages/index.js
+++ src/controllers/messages/index.js
@@ -223,14 +223,14 @@ module.exports = {
                                 option_8: "/files/8 - Gestão de Transporte.pdf",
                             };
                             const options = {
-                                option_1: `Segue PDF com maiores informações sobre: Nota Fiscal Produtor eletrônica (NFPe) \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!`,
-                                option_2: "Segue PDF com maiores informações sobre: Gestão Agricola \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
-                                option_3: "Segue PDF com maiores informações sobre: Gestão de Agroindústria \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
-                                option_4: "Segue PDF com maiores informações sobre: Gestão de Algodoeira \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
-                                option_5: "Segue PDF com maiores informações sobre: Gestão de Cerealista \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
-                                option_6: "Segue PDF com maiores informações sobre: Gestão de Armazenagem \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
-                                option_7: "Segue PDF com maiores informações sobre: Gestão de Cooperativa \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
-                                option_8: "Segue PDF com maiores informações sobre: Gestão de Transporte \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_1: `Segue PDF com maiores informações sobre: \nNota Fiscal Produtor eletrônica (NFPe) \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!`,
+                                option_2: "Segue PDF com maiores informações sobre: \nGestão Agricola \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_3: "Segue PDF com maiores informações sobre: \nGestão de Agroindústria \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_4: "Segue PDF com maiores informações sobre: \nGestão de Algodoeira \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_5: "Segue PDF com maiores informações sobre: \nGestão de Cerealista \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_6: "Segue PDF com maiores informações sobre: \nGestão de Armazenagem \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_7: "Segue PDF com maiores informações sobre: \nGestão de Cooperativa \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
+                                option_8: "Segue PDF com maiores informações sobre: \nGestão de Transporte \n\nA ABIGS agradece o seu contato, tenha um ótimo evento!",
                             }
                             const optionsNames = {
                                 option_1: "Nota Fiscal Produtor eletrônica (NFPe)",
